a40ebe4b248b89cbfebaed91f1b50ea8410cd2eb
mottimalka <117903589+mottimalka@users.noreply.github.com>
2024-01-21 22:52:46 +0200

    Add Bing search API integration

--- src/features/chat/chat-services/chat-api-simple.ts
+++ src/features/chat/chat-services/chat-api-simple.ts
@@ -27,13 +27,49 @@ export const ChatAPISimple = async (props: PromptGPTProps) => {
   const topHistory = history.slice(history.length - 30, history.length);
 
   try {
+    
+    // Set headers for bing search api
+    let headersList = {
+      "Ocp-Apim-Subscription-Key": process.env.AZURE_BING_SEARCH_API_KEY || "",
+      "mkt": "he-IL"
+    }
+
+    // Call bing search api
+    let bing_response = await fetch(process.env.AZURE_BING_SEARCH_ENDPOINT+"/v7.0/search?q=" + lastHumanMessage.content, {
+      method: "GET",
+      headers: headersList
+    });
+
+    // Convert bing response to text
+    let data = await bing_response.text();
+
+    // convert webpages to json object
+    const webpagesResponse = JSON.parse(data);
+
+    // Create an array to store custom object list (name, url, content)
+    const resultsPrompts = [];
+
+    // Check if webpages is not empty
+    if (webpagesResponse.webPages.value.length > 0) {
+      // Iterate through each webpage and get the snippet
+      for (const webpage of webpagesResponse.webPages.value) {
+        resultsPrompts.push(`name: ${webpage.name}\nurl: ${webpage.url}\ncontent: ${webpage.snippet}`);
+        break
+      }
+    }
+
     const response = await openAI.chat.completions.create({
       messages: [
         {
           role: "system",
           content: `-You are ${AI_NAME} who is a helpful AI Assistant.
           - You will provide clear and concise queries, and you will respond with polite and professional answers.
-          - You will answer questions truthfully and accurately.`,
+          - You will answer questions truthfully and accurately.
+          - Use the following sources to answer the question, 
+            If the information provided to you contains information that is not 
+            relevant to the user's question, do not convey this information in
+            your answer but only information that is relevant to the user's question,
+            here are the sources of the information {${resultsPrompts.join("\n")}}}`,
         },
         ...topHistory,
       ],
